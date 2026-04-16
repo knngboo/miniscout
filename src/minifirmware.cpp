8161e3f795d1b7cf4882b3a834e429eccc43d0b8
orlando <216954379+knngboo@users.noreply.github.com>
2026-04-16 09:17:52 -0500

    documented and declared pin modes + fixed all lowercase pin names

--- src/minifirmware.cpp
+++ src/minifirmware.cpp
@@ -1,5 +1,5 @@
-// --- minifirmware.cpp --- //
-
+//// --- minifirmware.cpp --- ////
+//// //////////////////////// ////
 // --- Includes --- //
 // //////////////// //
 #include <WiFi.h>
@@ -7,35 +7,36 @@
 
 // --- ESP32 Pin Names --- //
 // /////////////////////// //
-
-// I2C & Multiplexer
-#define I2C_SDA 21 //SDA -> Pin 21 [Serial Data]
-#define I2C_SCL 22 //SCL -> Pin 22 [Serial Clock]
+// GPIO PINS: I2C & Multiplexer
 #define MUX_RST 4 //RST -> Pin 4 [Multiplexer Reset]
 
-// ToF Sensor Channels
-#define MUX_CH_LEFT 0 // (SC0 / SD0)
-#define MUX_CH_CENTER 1 // (SC1 / SD1)
-#define MUX_CH_RIGHT 2 // (SC2 / SD2)
+// I2C COMMUNICATION: Control hand over to internal I2C Hardware Controller
+#define I2C_SDA 21 //SDA -> Pin 21 [Serial Data]
+#define I2C_SCL 22 //SCL -> Pin 22 [Serial Clock]
 
-// Left Motor Driver Channel B
-#define MTR_LPWM 15 //PWMB -> Pin 15 [PWM for Speed Control]
-#define MTR_L_IN1 16 //BIN1 -> Pin RX2 [Direction Control 1]
+// CHANNELS: Multiplexer channels
+#define MUX_CH_LEFT 0 // (sc0 / sd0)
+#define MUX_CH_CENTER 1 // (sc1 / sd1)
+#define MUX_CH_RIGHT 2 // (sc2 / sd2)
+
+// GPIO PINS: left motor driver channel b
+#define MTR_LPWM 15 //pwmb -> pin 15 [pwm for speed control]
+#define MTR_L_IN1 16 //bin1 -> pin rx2 [direction control 1]
 #define MTR_L_IN2 17 //BIN2 -> Pin TX2 [Direction Control 2]
 #define MTR_L_ENC_A 33 //C1 -> Pin 33 [Speed/Position A] *Input Only*
 #define MTR_L_ENC_B 32 //C2 -> Pin 32 [Speed/Position B] *Input Only*
 
-// Right Motor Driver Channel A
+// GPIO PINS: Right Motor Driver Channel A
 #define MTR_RPWM 5 //PWMA -> Pin 5 [PWM for Speed Control]
 #define MTR_R_IN1 18 //AIN1 -> Pin 18 [Direction Control 1]
 #define MTR_R_IN2 19 //AIN2 -> Pin 19 [Direction Control 2]
 #define MTR_R_ENC_A 35 //C1 -> Pin 35 [Speed/Position A] *Input Only*
 #define MTR_R_ENC_B 34 //C2 -> Pin 34 [Speed/Position B] *Input Only*
 
-// Steering Servo
+// GPIO PINS: Steering Servo
 #define SERVO_STEER 13 //PWM -> Pin 13 [PWM for Steering Control]
 
-// Buzzer
+// GPIO PINS: Buzzer
 #define BUZZER_PIN 27 //PWM -> Pin 27 [PWM for Buzzer Control]
 
 // --- WiFi Credentials --- //
@@ -66,13 +67,50 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
     }
 }
 
+//// --- Void Setup --- ////
+//// ////////////////// ////
 void setup() {
-    Serial.begin(115200);
 
+    // --- Declare Pin Modes --- //
+    // Multiplexer Control
+    pinMode(MUX_RST, OUTPUT); //Set Reset Pin as an output
+
+    // Right Motor Control
+    pinMode(MTR_RPWM, OUTPUT); //Set RPWM as an output to control speed
+    pinMode(MTR_R_IN1, OUTPUT); //Set Right Direction 1 as an output
+    pinMode(MTR_R_IN2, OUTPUT); //Set Right Direction 2 as an output
+
+    // Left Motor Control
+    pinMode(MTR_LPWM, OUTPUT); //Set LPWM as an output to control speed
+    pinMode(MTR_L_IN1, OUTPUT); //Set Left Direction 1 as an output
+    pinMode(MTR_L_IN2, OUTPUT); //Set Left Direction 2 as an output
+
+    // Motor Encoders
+    pinMode(MTR_L_ENC_A, INPUT); //Set Left Encoder A as an input to listen for rotation pulses
+    pinMode(MTR_L_ENC_B, INPUT); //Set Left Encoder B as an input to listen for rotation pulses
+    pinMode(MTR_R_ENC_A, INPUT); //Set Right Encoder A as an input to listen for rotation pulses
+    pinMode(MTR_R_ENC_B, INPUT); //Set Right Encoder B as an input to listen for rotation pulses
+
+    // Servo
+    pinMode(SERVO_STEER, OUTPUT); //Set Servo Pin as an output to send angle position
+
+    //Buzzer
+    pinMode(BUZZER_PIN, OUTPUT); //Set Buzzer Pin as an output to trigger audio
+
+    // --- Perform a Hrad Reset on I2C --- //
+    digitalWrite(MUX_RST, LOW);
+    delay(10);
+    digitalWrite(MUX_RST, HIGH);
+    delay(10);
+
+    Serial.begin(115200); //Starts serial communication to computer for debugging
+
+    // --- Start the WiFi Access Point --- //
     WiFi.softAP(ssid, password);
-    Serial.println("AP Started: " + String(ssid));
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.softAPIP());
+    Serial.println("AP Started: " + String(ssid)); //{serial debugging}
+    Serial.print("IP Address: "); //{serial debugging}
+    Serial.println(WiFi.softAPIP()); //{serial debugging}
+
 
     ws.onEvent(onEvent);
     server.addHandler(&ws);
