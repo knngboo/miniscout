f3d60daf8379db4934058dd415830d45d33425ad
orlando <216954379+knngboo@users.noreply.github.com>
2026-04-20 21:37:10 -0500

    spaced out the comments from the code

--- src/minifirmware.cpp
+++ src/minifirmware.cpp
@@ -4,9 +4,9 @@
 // --- Includes --- //
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <Wire.h> // Multiplexer Library
-#include <Adafruit_VL53L0X.h> // Sensors Library
-#include <ESPmDNS.h> // Domain Name Library [miniscout.local]
+#include <Wire.h>                   // Multiplexer Library
+#include <Adafruit_VL53L0X.h>       // Sensors Library
+#include <ESPmDNS.h>                // Domain Name Library [miniscout.local]
 
 // --- Global Object -- //
 AsyncWebServer server(80);
@@ -15,34 +15,34 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 // --- ESP32 Pin Names --- //
 // I2C & Multiplexer
-#define MUX_RST 4 // Multiplexer Reset
-#define I2C_SDA 21 // Serial Data
-#define I2C_SCL 22 // Serial Clock
+#define MUX_RST 4                   // Multiplexer Reset
+#define I2C_SDA 21                  // Serial Data
+#define I2C_SCL 22                  // Serial Clock
 
 // Multiplexer Channels
-#define MUX_CH_LEFT 0 // (sc0 / sd0)
-#define MUX_CH_CENTER 1 // (sc1 / sd1)
-#define MUX_CH_RIGHT 2 // (sc2 / sd2)
+#define MUX_CH_LEFT 0               // (sc0 / sd0)
+#define MUX_CH_CENTER 1             // (sc1 / sd1)
+#define MUX_CH_RIGHT 2              // (sc2 / sd2)
 
 // Left Motor Driver (Channel B)
-#define MTR_LPWM 15 // PWM for Speed Control
-#define MTR_L_IN1 16 // Direction Control 1
-#define MTR_L_IN2 17 // Direction Control 2
-#define MTR_L_C1 33 // Speed/Position A *Input Only*
-#define MTR_L_C2 32 // Speed/Position B *Input Only*
+#define MTR_LPWM 15                 // PWM for Speed Control
+#define MTR_L_IN1 16                // Direction Control 1
+#define MTR_L_IN2 17                // Direction Control 2
+#define MTR_L_C1 33                 // Speed/Position A *Input Only*
+#define MTR_L_C2 32                 // Speed/Position B *Input Only*
 
 // Right Motor Driver (Channel A)
-#define MTR_RPWM 5 // PWM for Speed Control
-#define MTR_R_IN1 18 // Direction Control 1
-#define MTR_R_IN2 19 // Direction Control 2
-#define MTR_R_C1 35 // Speed/Position A *Input Only*
-#define MTR_R_C2 34 // Speed/Position B *Input Only*
+#define MTR_RPWM 5                  // PWM for Speed Control
+#define MTR_R_IN1 18                // Direction Control 1
+#define MTR_R_IN2 19                // Direction Control 2
+#define MTR_R_C1 35                 // Speed/Position A *Input Only*
+#define MTR_R_C2 34                 // Speed/Position B *Input Only*
 
 // GPIO PINS: Steering Servo
-#define SERVO_PWM 13 // PWM for Steering Control
+#define SERVO_PWM 13                // PWM for Steering Control
 
 // GPIO PINS: Buzzer
-#define BUZZER_PWM 27 // PWM for Buzzer Control
+#define BUZZER_PWM 27               // PWM for Buzzer Control
 
 // --- WiFi Credentials --- //
 // //////////////////////// //
@@ -124,22 +124,22 @@ const char index_html[] PROGMEM = R"rawliteral(
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
     // --- Filter: Listen for Data only --- //
     if (type == WS_EVT_DATA) {
-        String message = ""; // Create Empty Container
+        String message = "";                    // Create Empty Container
         // --- Reconstruct Message --- //
         // Converts raw bits into readable text characters
         for (size_t i = 0; i < len; i++) {
-            message += (char)data[i]; // {Data Reassembly}
+            message += (char)data[i];           // Data Reassembly
         }
-        Serial.print("Command Received: "); // {Debug: Received}
-        Serial.println(message); // {Debug: Command}
+        Serial.print("Command Received: ");     // {Debug: Received}
+        Serial.println(message);                // {Debug: Command}
 
         // --- Command Switchboard --- //
         if (message == "SCOUT") {
-            Serial.println("Scouting Started..."); // [Trigger Drive Logic]
+            Serial.println("Scouting Started...");          // [Trigger Drive Logic]
         } else if (message == "STOP") {
-            Serial.println("Scouting Stopped..."); // [Kill Motors]
+            Serial.println("Scouting Stopped...");          // [Kill Motors]
         } else if (message == "RESET") {
-            Serial.println("Map Reset..."); // [Refresh Web UI]
+            Serial.println("Map Reset...");                 // [Refresh Web UI]
         }
     }
 }
@@ -150,66 +150,66 @@ void setup() {
 
     // --- Declare Pin Modes --- //
     // Multiplexer Control
-    pinMode(MUX_RST, OUTPUT); // Set Reset Pin
+    pinMode(MUX_RST, OUTPUT);           // Set Reset Pin
 
     // Right Motor Control
-    pinMode(MTR_RPWM, OUTPUT); // Set RPWM
-    pinMode(MTR_R_IN1, OUTPUT); // Set Right Direction 1
-    pinMode(MTR_R_IN2, OUTPUT); // Set Right Direction 2
+    pinMode(MTR_RPWM, OUTPUT);          // Set RPWM
+    pinMode(MTR_R_IN1, OUTPUT);         // Set Right Direction 1
+    pinMode(MTR_R_IN2, OUTPUT);         // Set Right Direction 2
 
     // Left Motor Control
-    pinMode(MTR_LPWM, OUTPUT); // Set LPWM
-    pinMode(MTR_L_IN1, OUTPUT); // Set Left Direction 1
-    pinMode(MTR_L_IN2, OUTPUT); // Set Left Direction 2
+    pinMode(MTR_LPWM, OUTPUT);          // Set LPWM
+    pinMode(MTR_L_IN1, OUTPUT);         // Set Left Direction 1
+    pinMode(MTR_L_IN2, OUTPUT);         // Set Left Direction 2
 
     // Right Motor Encoders
-    pinMode(MTR_R_C1, INPUT); //Set Right Encoder A
-    pinMode(MTR_R_C2, INPUT); //Set Right Encoder B 
+    pinMode(MTR_R_C1, INPUT);           //Set Right Encoder A
+    pinMode(MTR_R_C2, INPUT);           //Set Right Encoder B 
     
     // Left Motor Encoders
-    pinMode(MTR_L_C1, INPUT); // Set Left Encoder A
-    pinMode(MTR_L_C2, INPUT); //Set Left Encoder B          
+    pinMode(MTR_L_C1, INPUT);           // Set Left Encoder A
+    pinMode(MTR_L_C2, INPUT);           //Set Left Encoder B          
 
     // Servo
-    pinMode(SERVO_PWM, OUTPUT); //Set Servo Pin
+    pinMode(SERVO_PWM, OUTPUT);         //Set Servo Pin
 
     //Buzzer
-    pinMode(BUZZER_PWM, OUTPUT); //Set Buzzer Pin
+    pinMode(BUZZER_PWM, OUTPUT);        //Set Buzzer Pin
 
     // --- Perform a Hard Reset on I2C --- //
-    digitalWrite(MUX_RST, LOW); // Off
+    digitalWrite(MUX_RST, LOW);         // Off
     delay(10);
-    digitalWrite(MUX_RST, HIGH); // On
+    digitalWrite(MUX_RST, HIGH);        // On
     delay(10);
 
     // --- Serial Monitor Debugging --- //
-    Serial.begin(115200); // Start Data Link [115200]
+    Serial.begin(115200);               // Start Data Link [115200]
 
     // --- Network Hub --- //
-    WiFi.softAP(ssid, password); // Boot "vortex" WiFi
-    Serial.print("AP Started: "); // {debug}
-    Serial.println(ssid); // {debug}
-    Serial.print("IP Address: "); // {debug}
-    Serial.println(WiFi.softAPIP()); // {debug}
+    WiFi.softAP(ssid, password);        // Boot "vortex" WiFi
+    Serial.print("AP Started: ");       // {debug}
+    Serial.println(ssid);               // {debug}
+    Serial.print("IP Address: ");       // {debug}
+    Serial.println(WiFi.softAPIP());    // {debug}
 
     // --- DNS Router --- //
-    if (!MDNS.begin("miniscout")) { // Boot mDNS
-        Serial.println("Error setting up MDNS responder!"); // {debug}
+    if (!MDNS.begin("miniscout")) {     // Boot mDNS
+        Serial.println("Error setting up MDNS responder!");                     // {debug}
     } else {
-        Serial.println("mDNS responder started at http://miniscout.local"); // {debug}
+        Serial.println("mDNS responder started at http://miniscout.local");     // {debug}
     }
 
     // --- System Communication --- //
-    ws.onEvent(onEvent); // Bind Socket Logic
-    server.addHandler(&ws); // Enable WebSocket Support
+    ws.onEvent(onEvent);            // Bind Socket Logic
+    server.addHandler(&ws);         // Enable WebSocket Support
 
     // --- Web Routes Lambda Function --- //
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(200, "text/html", index_html); // Root Response [Send PROGMEM HTML]
+        request->send(200, "text/html", index_html);                // Root Response [Send PROGMEM HTML]
     });
-    server.begin(); // Launch Server
+    server.begin();         // Launch Server
 }
 
 void loop() {
-    ws.cleanupClients(); // Keep Socket Stable
+    ws.cleanupClients();    // Keep Socket Stable
 }
